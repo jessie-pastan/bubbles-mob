6f34995eb16c7972086da6a1b796904aebe82881
jessie-pastan <jessie.pastan@gmail.com>
2023-07-05 14:12:47 -0400

    Initial Commit

---
+++ Bubbles/BubblesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubblesApp.swift
+//  Bubbles
+//
+//  Created by Jessie P on 7/5/23.
+//
+
+import SwiftUI
+
+@main
+struct BubblesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
